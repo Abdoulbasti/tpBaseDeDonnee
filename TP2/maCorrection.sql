4097814246925d889aa9ebe8d0b7db5c174470f9
Abdoulbasti <mukailaabdoulbassit@gmail.com>
2023-02-15 12:33:49 +0100

    Requêtes avec jointure

--- TP2/maCorrection.sql
+++ TP2/maCorrection.sql
@@ -26,16 +26,57 @@
 
 --2 Requêtes avec jointure 
 
-    --1
+    --1 
+    -- Ajout de DISTINCT pour eviter les repetition de données
+    SELECT nom FROM equipes, matchs WHERE equipes.eid = matchs.gagnant;
 
+    SELECT nom FROM equipes JOIN matchs ON equipes.eid = matchs.gagnant;
 
     --2
+    SELECT tournois.nom, tournois.annee FROM tournois, matchs, equipes 
+    WHERE tournois.tid = matchs.tournois AND 
+            equipes.eid = 2 AND 
+            equipes.eid = matchs.perdant;
+
+    SELECT tournois.nom, tournois.annee 
+    FROM (tournois JOIN matchs ON tournois.tid = matchs.tournois)
+    JOIN equipes ON (equipes.eid = matchs.perdant AND equipes.eid = 2);
 
     --3
+    SELECT mid FROM matchs, equipes
+    WHERE matchs.perdant = equipes.eid AND equipes.nom = 'Wallabies';
+
+    SELECT mid FROM matchs JOIN equipes ON
+    (matchs.perdant = equipes.eid AND equipes.nom = 'Wallabies');
 
     --4
+    SELECT mid FROM matchs, equipes, participation 
+    WHERE
+        participation.eid = equipes.eid AND 
+        participation.tid = matchs.tournois AND
+        (participation.eid = matchs.gagnant OR participation.eid = matchs.perdant) AND
+        equipes.nom = 'All Blacks';
 
+
+    SELECT mid FROM (participation NATURAL JOIN equipes) 
+    JOIN matchs ON 
+    (
+        participation.tid = matchs.tournois AND 
+        (participation.eid = matchs.gagnant OR participation.eid = matchs.perdant) AND
+        equipes.nom = 'All Blacks'
+    );
+    
     --5
+    SELECT equipes.eid FROM equipes, participation, tournois
+    WHERE   tournois.nom = 'Coupe du Monde' AND 
+            tournois.annee = 1991 AND 
+            participation.tid = tournois.tid AND
+            participation.eid = equipes.eid;
+
+
+    SELECT DISTINCT equipes.eid FROM participation NATURAL JOIN equipes, tournois
+    WHERE   tournois.nom = 'Coupe du Monde' AND 
+            tournois.annee = 1991;
 
 
 --3 Requêtes avec double jointure 
